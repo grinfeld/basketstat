d62b15975c417d718a36c632e7c5afd7608065b4
Oleg <41973977+K-Oleg@users.noreply.github.com>
2019-03-02 19:18:02 +0200

    update driver to support timezone

--- run.cmd
+++ run.cmd
@@ -1,1 +1,1 @@
-java -DUSER=root -DPASSWORD=1946 -DDB_URL=localhost:3306/basketstat -jar basketstat-1.0.jar
+java -DUSER=root -DPASSWORD=1946 -DDB_URL="localhost:3306/basketstat?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC" -jar basketstat-1.0.jar
